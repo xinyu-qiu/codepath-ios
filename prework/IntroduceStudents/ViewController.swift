c05068e3d7fba3284e822e24129e600659806275
xinyu-qiu <113216096+xinyu-qiu@users.noreply.github.com>
2023-01-04 17:06:29 -0800

    finish init

---
+++ prework/IntroduceStudents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroduceStudent
+//
+//  Created by 邱欣宇 on 1/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
